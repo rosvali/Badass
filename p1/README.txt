a99d11a626f7477397a195597c177243183acce7
Rosalie AGUILLON <raguillo@e1r6p10.clusters.42paris.fr>
2022-09-13 11:46:56 +0200

    add notes for p1

--- p1/README.txt
+++
@@ -1,4 +0,0 @@
-Project: 'p1-1' created on 2022-09-12
-Author: John Doe <john.doe@example.com>
-
-No project description was given
--- p1/build.sh
+++ p1/build.sh
@@ -1,3 +1,5 @@
+# pull alpine image directly from dockerhub
 docker pull alpine:latest
 
+# build an image from a dockerfile and named it frrouting/frr
 docker build -t frrouting/frr /home/vagrant/Desktop/badass/p1
